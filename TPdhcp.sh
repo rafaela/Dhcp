ad3fb9c0c4aeae1497fabbae6e5f7677af5b38c0
Rafaela <raafapaiva19@gmail.com>
2019-12-03 23:57:53 -0200

    Pegando o endereço mac

--- TPdhcp.sh
+++ TPdhcp.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
-nome=$1
+ipProxy=$1
+senha=$2
+nome=$3
 dns="8.8.8.8,\ 8.8.4.4"
 tempo1="default-lease-time 300;"
 tempo2="max-lease-time 1800;"
-cliente=$2
-ip1="192.168.10.2"
-servidor=$3
-ip2="192.168.10.10"
+ipCliente="192.168.10.2"
+ipServidor="192.168.10.10"
 rede="subnet 192.168.10.0 netmask 255.255.255.240"
 range="range 192.168.10.101 192.168.10.199;"
 routers="option routers 192.168.10.200"
 
-#subnet 192.168.10.0 netmask 255.255.255.0 {
-#  range 192.168.10.101 192.168.10.199;
-#  option routers 192.168.10.100;
-#}
+sshpass -p "123456" ssh -o StrictHostKeyChecking=no root@$ipProxy 'systemctl status dhcpd | grep "active (running)"'
 
-#verifica se o serviço está executando
-if pgrep "dhcpd" > /dev/null
+if [ $? -eq 0 ]
 then
-    echo "O DHCP está executando"
-    #elimina linhas em branco e comentários do arquivo
+    echo "O DHCP está executando."
+    sshpass -p "$senha" ssh -o StrictHostKeyChecking=no root@$ipProxy ping -c 2 $ipServidor
+    sshpass -p "123456" ssh -o StrictHostKeyChecking=no root@$ipProxy ping -c 2 $ipCliente
+
+    cliente=$(sshpass -p "123456" ssh -o StrictHostKeyChecking=no root@$ipProxy ip neighbour show | grep $ipCliente | awk '{ print $5 }')
+
+    servidor=$(sshpass -p "123456" ssh -o StrictHostKeyChecking=no root@$ipProxy ip neighbour show | grep $ipServidor | awk '{ print $5 }')
+
+    sshpass -p "123456" scp -o StrictHostKeyChecking=no root@$ipProxy:/etc/dhcp/dhcpd.conf ./
+ 
     while read linha
     do
-        grep -v "^#" $linha | sed '/^$/d' > correcao.txt 2> /dev/null
-    done < /etc/dhcp/dhcpd.conf
+        grep -v "^#" $linha 2> /dev/null | sed /^$/d > correcao.txt
+    done < ./dhcpd.conf
     
     grep -q "authoritative" correcao.txt 2> /dev/null -a grep -q "log-facility local7" 2> /dev/null
     if [ $? -eq 0 ]
@@ -38,12 +42,12 @@ then
     grep -q $nome correcao.txt 2> /dev/null -a grep -q $dns correcao.txt 2> /dev/null
     if [ $? -eq 0 ]
     then
-	echo "As configurações de nome e DNS estão corretas"
-	pontos=`expr $pontos + 2`
+        echo "As configurações de nome e DNS estão corretas"
+        pontos=`expr $pontos + 2`
     else
-	echo "As configurações de nome e DNS estão incorretas"
+        echo "As configurações de nome e DNS estão incorretas"
     fi
-
+    
     grep -q $tempo1 correcao.txt 2> /dev/null
     if [ $? -eq 0 ]
     then
@@ -88,12 +92,14 @@ then
     else
         echo "A configuração de rede está incorreta"
     fi
-
+    
     echo "Sua pontuação nessa questão é: $pontos"
 
-   #apaga o arquivo auxiliar usado na correção
-   rm -rf correcao.txt
+    #apaga o arquivo auxiliar usado na correção
+    rm -rf correcao.txt
+    rm -rf ./dhcpd.conf
 
 else
-    echo "O DHCP não está executando. Você não irá pontuar na questão"
+    echo "O DHCP não está executando. Você não irá pontuar nessa questão."
 fi
+
